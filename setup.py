60c94cdae18eb6851bf7b2d45d63eccf169a41d5
Victor Ziv <victor@colabo.com>
2019-10-08 19:32:24 +0300

    Add metadata

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 here = os.path.dirname(__file__)
-
 
 with open(os.path.join(here, 'VERSION')) as vf:
     VERSION = vf.read().strip()
 
 setup(
-    name='hwinfrasdk',
+    name='hwsdk',
     version=VERSION,
     author="Victor Ziv",
-    author_email="vziv@infinidat.com",
-    packages=find_packages(),
+    author_email="ziv.victor@gmail.com",
+    url='https://github.com/victorziv/hwsdk',
+    packages=['hwsdk'],
     long_description=open('README.rst').read(),
     long_description_content_type="text/x-rst",
     install_requires=[
