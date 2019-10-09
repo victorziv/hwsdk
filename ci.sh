2afd245512e8e637bbe96bdf3001cafe770cb15f
Victor Ziv <victor@colabo.com>
2019-10-09 18:58:41 +0300

    Use twine environment variables to set upload parameters

--- ci.sh
+++ ci.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set_env() {
+    export TWINE_USERNAME=victorziv
+    export TWINE_PASSWORD=Bab1848uin
+    export TWINE_REPORSITORY=testpypy
+    export TWINE_REPOSITORY_URL=https://test.pypi.org/legacy/
+}
+# ______________________________________
+
 run_tests() {
     pytest -v tests/
     if [ $? -ne 0 ];then
@@ -15,6 +23,7 @@ publish() {
 # ______________________________________
 
 main() {
+    set_env
     run_tests
     publish
 }
